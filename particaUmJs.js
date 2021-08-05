8096824e073f1dfdf0eae89053b271ab1cb4008e
Sarah <sarahleticiasr@gmail.com>
2021-08-05 19:17:18 -0300

    versionando exercicios de js

---
+++ particaUmJs.js
@@ -0,0 +1,85 @@
+//OPERADORES ARITMETICOS
+
+//adição + 
+//var someDoisNumeros = 1+3
+//console.log(someDoisNumeros)
+
+//subtração -
+//var subDoisNumeros = 55 - 6
+//console.log(subDoisNumeros)
+
+//multiplicação *
+//var multTresNum = 6 * 5 * 9
+//console.log(multTresNum)
+
+//var divirNumeros = 78/78
+//console.log(divirNumeros)
+
+//modulo ou resto de uma divisão %
+//console.log(5%3)
+
+//incremento ++ (vai ser igual a numero = número + 1)
+//var numero = 57
+//console.log(++numero)
+
+//decremento -- (vai ser igual a numero = número - 1)
+//var number = 36
+//console.log(--number)
+
+
+// OPERADORES DE ATRIBUIÇÃO
+
+// O SINAL "=" SIGNIFICA ATRIBUIÇÃO
+
+//ATRIBUIÇÃO DE SOMA
+//var primeiroElememto = 5
+//var segundoElememto = 9
+//console.log(primeiroElememto += segundoElememto) //mesma coisa que x=x+y
+
+//para as outras operações a gente seguiria o mesmo raciocionio:
+//subtração : console.log(x-=y)
+//multiplicação : console.log(x*=y)
+//divisao : console.log(x/=y)
+//resto da divisão : console.log(x%=y)
+
+
+// OPERADORES DE COMPARAÇÃO
+
+// == COMPARAR VALOR
+// === COMPARAR VALOR E TIPO
+// != DIFERENTE DE 
+//< OU <= MENOR OU MENOR/IGUAL
+//> OU >= MAIOR OU MAIOR/IGUAL
+
+// OPERADORES LÓGICOS
+
+// && SIGNIFICA "E"
+// || SIGNIFICA "OU"
+// ! SIGNIFICA "NEGAÇÃO"
+
+// CONDICIONAIS IF E ELSE
+//SWITCH CASE
+
+//let mes = "março"
+//switch ( mes) {
+   // case "fevereiro":
+   // console.log("mes dois")
+   // break
+
+   // case "março":
+   // console.log("mes tres")
+   // break
+
+   // case "setembro":
+   // console.log("mes nove")
+   // break
+    
+   // default:
+   // console.log("Nenhum caso foi atendido")
+//}
+
+//ESTRUTURA DE REPETIÇÃO
+
+
+
+//
